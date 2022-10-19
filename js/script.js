5bd4a1fdab1aa7ec1756eb7b9997817cd664b1f3
Julia Carmo <93735857+JuliamCarmo@users.noreply.github.com>
2022-10-19 12:03:59 -0300

    código refatorado com javascript

--- js/script.js
+++ js/script.js
@@ -1,12 +1,44 @@
-const links = document.querySelectorAll(".header-menu a")
+//Ativar links do menu
+const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
-  const url = location.href
-  const href = link.href
+  const url = location.href;
+  const href = link.href;
 
   if (url.includes(href)) {
-    link.classList.add("ativo")
+    link.classList.add("ativo");
   }
 }
+links.forEach(ativarLink);
 
-links.forEach(ativarLink)
+//Ativar items do orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro)
+  if (elemento) {
+    elemento.checked = true
+  }
+}
+parametros.forEach(ativarProduto);
+
+//Perguntas Frequentes
+
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute('aria-controls');
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle('ativa');
+  const ativa = resposta.classList.contains("ativa");
+  console.log(ativa)
+  pergunta.setAttribute('aria-expanded', ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener('click', ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
